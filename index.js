6363be58951310099140f21805b855ba45f3b2cb
Aakanksha <109036438+Aakanksha777@users.noreply.github.com>
2023-01-02 22:43:35 +0530

    Add blogs

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const blogContainer = document.querySelector(".blog-container");
-const hiddenBlog = document.querySelector(".hide-blog");
-const readMoreBtn = document.querySelector(".read-more-btn");
-const closeBtn = document.querySelector(".close-btn");
+// const blogContainer = document.querySelector(".blog-container");
+// const hiddenBlog = document.querySelector(".hide-blog");
+// const readMoreBtn = document.querySelector(".read-more-btn");
+// const closeBtn = document.querySelector(".close-btn");
 
-function showBlog () {
-    hiddenBlog.classList.remove("hide")
-    closeBtn.classList.remove("hide")
-    readMoreBtn.classList.add("hide")
-    console.log("Running the btn")
-}
-readMoreBtn.addEventListener("click", showBlog)
+// function showBlog () {
+//     hiddenBlog.classList.remove("hide")
+//     closeBtn.classList.remove("hide")
+//     readMoreBtn.classList.add("hide")
+//     console.log("Running the btn")
+// }
+// readMoreBtn.addEventListener("click", showBlog)
 
 
-function wrappedBlog () {
-    hiddenBlog.classList.add("hide")
-    closeBtn.classList.add("hide")
-    readMoreBtn.classList.remove("hide")
-    console.log("Running the btn")  
-}
+// function wrappedBlog () {
+//     hiddenBlog.classList.add("hide")
+//     closeBtn.classList.add("hide")
+//     readMoreBtn.classList.remove("hide")
+//     console.log("Running the btn")  
+// }
 
-closeBtn.addEventListener("click", wrappedBlog)
+// closeBtn.addEventListener("click", wrappedBlog)
